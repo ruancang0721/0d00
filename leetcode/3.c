97d161bc599bfd60f506c01928a8195b941066ba
ruancang <lb3335874469@163.com>
2024-11-24 00:08:28 +0800

    无重复字符的最长子串

--- leetcode/3.c
+++ leetcode/3.c
@@ -1,43 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main()
-{
-    char s[]="abcabcbb";
-    int len=lengthOfLongestSubstring(s);
-    printf("%d",len);
-}
+int lengthOfLongestSubstring(char* s) {
+   int l=0,r=0;
+   int hash[666]={0};
+   int len = strlen(s);
+   int ilen=0;
+   int max=1;
+   if(len==0)
+   return 0;
+   for(r=0;r<len;r++)
+   {
+    hash[s[r]]++;
+    ilen++;
+      while(hash[s[r]]>1)
+      {
+         hash[s[l]]--;
+         l++;
+         ilen--;
+      }
+         max=max>ilen?max:ilen;
+   }
 
-int lengthOfLongestSubstring(char* s)
- {
-   
-           int lenlen=strlen(s);
-           int max=0;
-           
-           for(int i=0;i<lenlen;i++)
-           {
-            int count=1;
-            if(i==lenlen-1)
-            {
-              max=max>1?max:1;
-              continue;
-            }
-            if(s[i]==s[i+1])
-            {
-               max=max>1?max:1;
-               continue;
-
-            }
-            for(int j=i+1;j<lenlen;j++)
-            {
-              if(s[i]==s[j])
-              {
-                break;
-              }
-              count++;
-            }
-              max=max>count?max:count;
-           }
-        
-    return max;
+return max;
 }
