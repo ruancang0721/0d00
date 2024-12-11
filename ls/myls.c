85c7aece1c83139692d860e731b96b8602bc8f3a
ruancang <lb3335874469@163.com>
2024-12-11 23:03:08 +0800

    myls

--- ls/myls.c
+++ ls/myls.c
@@ -1,17 +1,53 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<string.h>
 #include<stdlib.h>
-int main(int argc,char * argv[])
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<dirent.h>
+#include<sys/stat.h>
+
+
+
+void read_dir(char*dir)
 {
-    struct stat sbuf;
-    int ret = stat(argv[1],&sbuf);
-    if(ret  == -1)
+    char path[256];
+    DIR* dp;
+    struct dirent * dirp;
+    dp = opendir(dir);
+    while((dirp =readdir(dp))!=NULL)
+    {
+        sprintf(path,"%s/%s",dir,dirp->d_name);
+        isfile(path);
+    }
+    closedir(dp);
+
+    
+}
+void isfile(char* name)
+{
+    struct stat st;
+    int ret = stat(name,&st);
+    if(ret == -1)
     {
         perror("stat");
-        exit(1);
+        return;
     }
-    printf("%ld",sbuf.st_size);
+    if(S_ISDIR(st.st_mode))
+    {
+           read_dir(name);
+    }
+    printf("%s\t%ld\n",name,st.st_size);
+}
+
+int main(int argc,char* argv[])
+{
+
+   if(argc==1)
+   {
+    isfile(".");
+   }
+   else{
+    isfile(argv[1]);
+   }
     return 0;
 }
