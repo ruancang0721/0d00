2b5890316c7b5f74727e40f452beb9319e1b0b57
ruancang <lb3335874469@163.com>
2025-01-31 01:43:29 +0800

    myls

--- ls/myls.c
+++ ls/myls.c
@@ -6,25 +6,47 @@
 #include<dirent.h>
 #include<sys/stat.h>
 #include<sys/types.h>
-void ls(char * path)
+#include<getopt.h>
+typedef struct{
+  int a;
+  int l;
+  int R;
+  int t;
+  int r;
+  int i;
+  int s;
+}Options;
+typedef struct{
+  char* name;
+  struct stat st;
+}filei;
+void ls(char* path,Options* opts,int ll)
 {
     DIR * dir = opendir(path);
-    if(dir == NULL)
-    {
-        perror("open error");
+       if(dir == NULL)
+       {
+        perror("opendir error");
         return;
-    }
-      struct dirent * ptr;
-      while((ptr = readdir(dir))!=NULL)
-      {
-        if(ptr->d_name[0]!='.')
-      {
-       
-        printf("%s\n",ptr->d_name);
-      }
-      
-      }
-  
+       }
+       struct dirent *ent;
+       while((ent = readdir(dir))!=NULL)
+       {
+        if((opts->a!=1)&&ent->d_name[0]=='.')
+        {
+          continue;
+        }
+        char fullpath[1024];
+        snprintf(fullpath,1024,"%s/%s",path,ent->d_name);
+        struct stat st;
+        if(lstat(fullpath,&st)<0)
+        {
+          perror("lstat error");
+          continue;
+        }
+
+       }
+
+
 
 
     closedir(dir);
@@ -32,14 +54,66 @@ void ls(char * path)
 
 int main(int argc,char* argv[])
 {
-    char *path = ".";
+     Options options;
+     int opt;
+     while((opt=getopt(argc,argv,"alRrtsi"))!=-1)
+     {
+      switch(opt)
+      {
+        case 'a':
+        options.a=1;
+        break;
+        case 'l':
+        options.l=1;
+        break;
+        case 'R':
+        options.R=1;
+        break;
+        case 't':
+        options.t=1;
+        break;
+        case 'r':
+        options.r=1;
+        break;
+        case 'i':
+        options.i=1;
+        break;
+        case 's':
+        options.s=1;
+        break;
+        default:
+        perror("weizhi error");
+        return 1;
+
+      }
+     }
+     int dirnum = 0;
+     char **dir =NULL;
+     if(optind<argc)
+     {
+          dirnum = argc -optind;
+          dir = argv + optind;
+     }
+     if(dirnum == 0)
+     {
+        ls(".",&options,0);
+     }
+     else{
+      for(int i=0;i<dirnum;i++)
+      {
+        if(dirnum>1)
+        {
+          printf("%s:\n",dir[i]);
+
+        }
+        ls(dir[i],&options,0);
+        if(dirnum>1&&i!=dirnum-1)
+        {
+          printf("\n");
+        }
+      }
+     }
 
 
-
-    if(argc == 2)
-    {
-        path = argv[1];
-    }
-    ls(path);
     return 0;
 }
