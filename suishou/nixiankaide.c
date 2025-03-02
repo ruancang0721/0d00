32ca04661170aa157381ce3c567e2fcae506ff3d
ruancang <lb3335874469@163.com>
2025-03-02 20:16:04 +0800

    随手

--- suishou/nixiankaide.c
+++ suishou/nixiankaide.c
@@ -4,35 +4,42 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <wait.h>
 int main(int argc, char const *argv[])
 {
 
 pid_t wpid,pid ;
-int status;
-
-
-int i=0;
-
-
-pid = fork();
-if(pid ==0)
+int i;
+for(i=0;i<5;i++)
 {
-  printf("child,my pid is %d,sleep 10s",getpid());
-  sleep(10);
-}
-else if(pid>0)
-{
-  wpid = wait(&status);
-  if(wpid == -1)
+  pid = fork();
+  if(pid==0)
   {
-    perror("wait error");
-    exit(1);
+       break;
   }
-  printf("parent,my pid is %d,child pid is %d,child status is %d",getpid(),wpid,status);
 }
-else{
-  perror("fork error");
-  return 1;
+if(5==i)
+{
+    while((wpid = waitpid(-1,NULL,WNOHANG))!=-1)
+    {
+      if(wpid >0)
+      {
+        printf("wait child %d\n",wpid);
+      }
+      else if(wpid == 0)
+      {
+        sleep(1);
+      }
+    }
+
+
+
 }
+else
+{
+  sleep(i);
+  printf(" woshizijincheng %d\n",getpid());
+}
+
     return 0;
 }
