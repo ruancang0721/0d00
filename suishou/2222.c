9bc82cb6736c6a352b0fa89c5d56664d882a98ed
ruancang <lb3335874469@163.com>
2025-03-05 22:12:25 +0800

    suishou

--- suishou/2222.c
+++ suishou/2222.c
@@ -9,12 +9,25 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <pthread.h>
+char * myshe_read()
+{
+ char * line = NULL;
+  ssize_t bufsize;
+  getline(&line,&bufsize,stdin);
+  return line;
+
+
+
+
+
+
+}
 void mysh_loop()
 {
     char * line;
+     char** args;
 
-
-
+  int status;
 
 
     do{
@@ -22,16 +35,21 @@ void mysh_loop()
 
         char path[100];
         getcwd(path,100);
-        char now[200] = "[myshell";
+        char now[200] = "[ruancang-";
         strcat(now,path);
         strcat(now,"]$");
         printf("%s",now);
-    }
+
+        line = mysh_read();
+        args = mysh_split_line(line);
+           status = mysh_execute(args);
+           free(line);
+    }while(status);
 }
 int main(int argc, char *argv[])
 {
 
-
+mysh_loop();
 
 
 
